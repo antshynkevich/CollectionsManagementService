999c9f795b8338bd11b305f6e7fd0cace817bcb8
antonshinkevich <antons.hinkevich@gmail.com>
2024-05-10 16:18:50 +0300

    Add the ViewModel and the corresponding View to create a collection.

---
+++ CollectionsManagementService/Controllers/CollectionController.cs
@@ -0,0 +1,39 @@
+﻿using CollectionsManagementService.VievModels;
+using DataORMLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollectionsManagementService.Controllers;
+
+public class CollectionController : Controller
+{
+    [HttpGet]
+    public IActionResult Create()
+    {
+        var viewModel = new CreateCollectionViewModel();
+        int fieldsIndex = 0;
+        var fieldsArray = viewModel.CollectionFields;
+        foreach (var fieldType in (FieldType[]) Enum.GetValues(typeof(FieldType)))
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                fieldsArray[fieldsIndex] = new CreateCollectionFieldViewModel
+                {
+                    FieldType = fieldType
+                };
+
+                fieldsIndex++;
+            }
+        }
+
+        return View(viewModel);
+    }
+
+    [HttpPost]
+    public IActionResult Create(CreateCollectionViewModel viewModel)
+    {
+        // Process the submitted data (e.g., save to the database)
+        // viewModel.Name, viewModel.Description, viewModel.CategoryId, viewModel.CollectionFields, etc.
+        // Redirect to another page or return a view
+        return RedirectToAction("Index", "Home");
+    }
+}
---
+++ CollectionsManagementService/VievModels/CreateCollectionViewModel.cs
@@ -0,0 +1,19 @@
+﻿using DataORMLayer.Models;
+
+namespace CollectionsManagementService.VievModels;
+
+public class CreateCollectionViewModel
+{
+    public CreateCollectionFieldViewModel[] CollectionFields { get; set; } 
+        = new CreateCollectionFieldViewModel[3 * 5];
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int CategoryId { get; set; }
+}
+
+public class CreateCollectionFieldViewModel
+{
+    public FieldType FieldType { get; set; }
+    public string Name { get; set; }
+    public bool IsFieldNeeded { get; set; }
+}
